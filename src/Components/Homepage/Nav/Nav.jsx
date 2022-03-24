1011e0383f041a6eda939c38a944c7a77e8d7246
kunlefash <kunlefashmayop@gmail.com>
2022-03-24 12:28:50 -0300

    some changes to the navbar

--- src/Components/Homepage/Nav/Nav.jsx
+++ src/Components/Homepage/Nav/Nav.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import "./nav.css"
 const Nav = () => {
+  
   return (
-          <nav>
-              <div className='logo'>Logo</div>
-              <ul>
-                  <li className='navbar'><a href="#pro">Buycoins Pro</a></li>
-                  <li className='navbar'><a href="#about">About</a></li>
-                  <li className='navbar'><a href="#company">Company</a></li>
-                  <li className='navbar'><a href="#login">Login</a></li>
-                  <li className='navbar'><a href="#services">Services</a></li>
-              </ul>
-              <button type='button'>Get Started</button>
-          </nav>
+    <header>
+      <img className='logo' src="" alt="logo" />
+      <nav>
+        <ul className='nav_links'>
+          <li><a href="#">Buycoin Pro</a></li>
+          <li><a href="#Products">Products</a></li>
+          <li><a href="#Learn">Learn</a></li>
+          <li><a href="#Company">Company</a></li>
+          <li><a href="#Signin">Sign In</a></li>
+          <a className='cta'  href="#"><button>Get Started</button></a>
+
+
+        </ul>
+      </nav>
+    </header>
+
   )
 }
 
