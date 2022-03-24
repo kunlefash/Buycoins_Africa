f4663d4a3efc36f049572f2b48d2ffd0900551e9
kunlefash <kunlefashmayop@gmail.com>
2022-03-24 09:18:16 -0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import About from './Components/Homepage/About/About'
-import Hero from './Components/Homepage/Hero/Hero'
+import Hero1 from './Components/Homepage/Hero1/Hero1'
 import Nav from './Components/Homepage/Nav/Nav'
 
 const App = () => {
   return (
     <div>
       <Nav />
-      <Hero />
+      <Hero1 />
       <About/> 
     </div>
   )
---
+++ src/Components/Homepage/Hero1/Hero1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./hero1.css"
+const Hero = () => {
+  return (
+    <div className='container_hero1'>
+      <h1 className='h1_easy'>Trading made </h1>
+      <h1 className='h1_easy'>easy</h1>
+      <h4>Easily buy and sell assets like BTC, ETH, Tesla, Amazon </h4>
+      <h4>and many more with as little as N500</h4>
+      <button type='button'>Get Started Today</button>
+
+    </div>
+  )
+}
+
+export default Hero
