f16b2c71db00bcd10010688153c752aa132d59ec
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-03-25 09:07:34 +0100

    Navbar toggle fixed - resolve PR from branch-theo

--- src/Components/Homepage/Nav/Nav.jsx
+++ src/Components/Homepage/Nav/Nav.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import "./nav.css"
 import {AiOutlineMenu} from "react-icons/ai"
 
 function Nav()  {
+  const [toggle, setToggle] = useState(false)
+  const navRef = useRef()
+
+  const handleToggler = () => {
+    setToggle(!toggle)
+
+  }
+
+  useEffect(() => {
+    
+    // navRef.current.style.top = '-300px'
+    if(toggle){
+      navRef.current.style.top = '75px'
+      navRef.current.style.transition = '.4s top'
+    }else{
+      navRef.current.style.top = '-300px'
+      navRef.current.style.transition = '.4s top'
+    }
+      
+  }, [toggle])
+
   return (
     <header>
       <img className='logo' src="" alt="logo" />
       <nav>
-        <ul id='menuList' className='nav_links'>
+        <ul ref={navRef} id='menuList' className='nav_links'>
           <li><a href="#">Buycoin Pro</a></li>
           <li><a href="#Products">Products</a></li>
           <li><a href="#Learn">Learn</a></li>
           <li><a href="#Company">Company</a></li>
           <li><a href="#Signin">Sign In</a></li>
           <a className='cta'  href="#"><button>Get Started</button></a>
-          
-
-
         </ul>
       </nav>
-      <AiOutlineMenu className='menu_icon' onClick="togglemenu"/>
+      <AiOutlineMenu className='menu_icon' onClick={handleToggler}/>
 
     </header>
 
