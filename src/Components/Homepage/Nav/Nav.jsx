aa9aa72b74b61fe70c44c809a5cc44666b54da9d
kunlefash <kunlefashmayop@gmail.com>
2022-03-25 04:10:11 -0300

    navbar

--- src/Components/Homepage/Nav/Nav.jsx
+++ src/Components/Homepage/Nav/Nav.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import "./nav.css"
-const Nav = () => {
-  
+import {AiOutlineMenu} from "react-icons/ai"
+
+function Nav()  {
   return (
     <header>
       <img className='logo' src="" alt="logo" />
       <nav>
-        <ul className='nav_links'>
+        <ul id='menuList' className='nav_links'>
           <li><a href="#">Buycoin Pro</a></li>
           <li><a href="#Products">Products</a></li>
           <li><a href="#Learn">Learn</a></li>
           <li><a href="#Company">Company</a></li>
           <li><a href="#Signin">Sign In</a></li>
           <a className='cta'  href="#"><button>Get Started</button></a>
+          
 
 
         </ul>
       </nav>
+      <AiOutlineMenu className='menu_icon' onClick="togglemenu"/>
+
     </header>
 
   )
